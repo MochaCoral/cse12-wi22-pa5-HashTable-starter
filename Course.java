ac0f86801183d26432ac3619f49b43169c326b1a
MochaCoral <90062849+MochaCoral@users.noreply.github.com>
2022-02-17 02:12:12 -0800

    Finished implementation of Course class

--- Course.java
+++ Course.java
@@ -4,6 +4,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class Course {
     HashSet<Student> enrolled;
@@ -13,56 +14,96 @@ public class Course {
     private final String description;
 
     public Course(String department, String number, String description, 
-        int capacity){}
+        int capacity) throws IllegalArgumentException {
+            this.enrolled = new HashSet<Student>(0);
+            this.capacity = capacity;
+            this.department = department;
+            this.number = number;
+            this.description = description;
+    }
 
     public String getDepartment(){
-        return null;
+        return this.department;
     }
 
     public String getNumber(){
-        return null;
+        return this.number;
     }
 
     public String getDescription(){
-        return null;
+        return this.description;
     }
 
     public int getCapacity(){
-        return 0;
+        return this.capacity;
     }
 
-    public boolean enroll(Student student) {
-        return false;
+    public boolean enroll(Student student) throws IllegalArgumentException {
+        if(student == null) {
+            throw new IllegalArgumentException();
+        }
+        else if(this.enrolled.size() < this.capacity && this.enrolled.contains(student)) {
+            this.enrolled.add(student);
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
-    public boolean unenroll(Student student) {
-        return false;
+    public boolean unenroll(Student student) throws IllegalArgumentException {
+        if(student == null) {
+            throw new IllegalArgumentException();
+        }
+        else if(this.enrolled.contains(student)) {
+            this.enrolled.remove(student);
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
-    public void cancel() {}
+    public void cancel() {
+        this.enrolled.clear();
+    }
 
     public boolean isFull() {
-        return false;
+        if(this.enrolled.size() == this.getCapacity) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     public int getEnrolledCount() {
-        return 0;
+        return this.enrolled.size();
     }
 
     public int getAvailableSeats() {
-        return 0;
+        return this.getCapacity - this.enrolled.size();
     }
 
     public HashSet<Student> getStudents() {
-        return null;
+        return this.enrolled;
     }
 
     public ArrayList<Student> getRoster() {
-        return null;
+        ArrayList<Student> roster = new ArrayList<Student>
+        Iterator<Student> enrolledIterator = this.enrolled.iterator();
+        while(roster.size() < enrolled.size()) {
+            roster.add(enrolledIterator.next());
+        }
+        Collections.sort(roster);
+        return roster;
     }
 
     public String toString() {
-        return null;
+        String rep = this.getDepartment + " " + this.getNumber 
+            + " [" + this.getCapacity + "]" 
+            + "\n" + this.getDescription;
+        return rep;
     }
 }
 
