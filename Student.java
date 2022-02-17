df10c4599ace1092cb1ce74e6873a348dc4a6bad
MochaCoral <90062849+MochaCoral@users.noreply.github.com>
2022-02-17 00:18:56 -0800

    finished student class implementation

--- Student.java
+++ Student.java
@@ -8,32 +8,59 @@ public class Student implements Comparable<Student> {
     private final String lastName;
     private final String PID;
 
-    public Student(String firstName, String lastName, String PID) {}
+    public Student(String firstName, String lastName, String PID) throws IllegalArgumentException {
+        if(firstName == null || lastName == null || PID == null) {
+            throw new IllegalArgumentException();
+        }
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.PID = PID
+
+    }
 
     public String getLastName() {
-        return null;
+        return this.lastName;
     }
 
     public String getFirstName() {
-        return null;
+        return this.firstName;
     }
 
     public String getPID() {
-        return null;
+        return this.PID;
     }
 
     @Override
     public boolean equals(Object o) {
-        return false;
+        if(o instanceof Student) { 
+            if(this.getFirstName == o.getFirstName && this.getLastName == o.getLastName 
+                && this.getPID == o.getPID) {
+                return true;
+            }
+        }
+        else {
+            return false;
+        }
     }
 
     @Override
     public int hashCode() {
-        return 0;
+        return Objects.hash(this.getFirstName, this.getLastName, this.getPID);
     }
 
     @Override
     public int compareTo(Student o) {
-        return 0;
+        if(this.getLastName =! o.getLastName) {
+            return this.getLastName.compareTo(o.getLastName);
+        }
+        else if(this.getFirstName =! o.getFirstName) {
+            return this.getFirstName.compareTo(o.getFirstName);
+        }
+        else if(this.getPID =! o.getPID) {
+            return this.getPID.compareTo(o.getPID);
+        }
+        else {
+            return 0;
+        }
     }
 }
